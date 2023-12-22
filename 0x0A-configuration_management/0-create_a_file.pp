2e6ee074c2dff30a908b79e30134c27c1b8e90a7
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-22 13:08:31 +0000

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,9 @@
+#create a file school in temp
 
+file { '/tmp/school':
+  ensure    => file,
+  content   => 'I love pup',
+  mode      => '0744',
+  owner     => 'www-data',
+  group     => 'www-data',
+}
