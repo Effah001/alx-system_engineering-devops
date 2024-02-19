e3faecfd8170dfe3d07d66fc164945feca6d1414
Effah001 <effah210@gmail.com>
2024-02-19 06:07:10 +0000

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ Fetch data from JSONPlaceholder API based on person ID
 import requests
 import sys
 
+
 def fetch_todo_list(user_id):
     """Fetch and display employee TODO list progress"""
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
@@ -16,24 +17,24 @@ def fetch_todo_list(user_id):
 
     if list_user.status_code == 200:
         user = list_user.json()
-        user_name = user['name']
+        user = user['name']
 
         todos = list_todos.json()
-        total_tasks = 0
-        completed_tasks = 0
+        total = 0
+        done = 0
         completed_task_titles = []
 
         for task in todos:
-            total_tasks += 1
+            total += 1
             if task['completed']:
-                completed_tasks += 1
+                done += 1
                 completed_task_titles.append(task['title'])
 
-        print(f"Employee {user_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+        print(f"Employee {user} is done with tasks ({done}/{total}):")
         for title in completed_task_titles:
             print(f"\t{title}")
+
 
 if __name__ == "__main__":
     user_id = int(sys.argv[1])
     fetch_todo_list(user_id)
-
