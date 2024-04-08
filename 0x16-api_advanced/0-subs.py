7e527586c178e5b85b0061134e702a4cd9cd7bf1
Effah001 <effah210@gmail.com>
2024-04-08 22:57:13 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    user_agent = {'user_agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'}
+    user_agent = {'User_Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
+                  'rv: 123.0) Gecko/20100101 Firefox/123.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=user_agent, allow_redirects=False)
 
