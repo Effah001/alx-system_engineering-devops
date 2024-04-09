e243b5e8850428597635138d996b61659000da8c
Droid <44108804+Effah001@users.noreply.github.com>
2024-04-09 17:13:30 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
                   'rv: 123.0) Gecko/20100101 Firefox/123.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
@@ -13,9 +16,7 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
