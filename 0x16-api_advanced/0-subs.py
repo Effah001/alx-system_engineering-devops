3d59294e6db9d37bccb89acfb234997b855eaa60
Droid <44108804+Effah001@users.noreply.github.com>
2024-04-10 06:54:53 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;'
                   'rv: 123.0) Gecko/20100101 Firefox/123.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    response = requests.get(url, headers=user_agent)
 
     if response.status_code == 200:
         data = response.json()
