a46c0bd47b4fd45428b378f62da19121362afae5
Droid <44108804+Effah001@users.noreply.github.com>
2024-04-10 01:59:24 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" Get the hot posts of a subreddit"""
+
+"""API request to reddit for first 10 hot post"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Get the top 10 posts"""
-    url = "https://www.reddit.com/r/" + subreddit + "/top.json"
-    headers = {"User-Agent": "TopHotPost/1.0 {by Wills}"}
-    res = requests.get(url, headers=headers)
-    if (res.status_code == 200):
-        data = res.json()["data"]
-        data = data["children"][:10]
-        for post in data:
-            print(post["data"]["title"])
-        return
-    print("None")
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;'
+                  'rv: 123.0) Gecko/20100101 Firefox/123.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        top_ten_posts = [post['data']['title'] for post in posts[:10]]
+        return top_ten_posts
+    else:
+        print("None")
