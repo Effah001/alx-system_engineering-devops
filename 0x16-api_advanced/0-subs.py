a4998119d71f26fbe43ddb92c774f1dfcf9132d6
Droid <44108804+Effah001@users.noreply.github.com>
2024-04-10 06:58:20 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,6 @@ import requests
 def number_of_subscribers(subreddit):
     """count subreddit subscribers"""
 
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
     user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;'
                   'rv: 123.0) Gecko/20100101 Firefox/123.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
