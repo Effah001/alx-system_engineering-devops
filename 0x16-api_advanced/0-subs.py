35127564356a41be9ee2e0ac4ada7a4748d0fc24
Droid <44108804+Effah001@users.noreply.github.com>
2024-04-10 07:00:48 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
-"""API request to reddit for subscribers"""
-
+"""
+Get the subscribers count of a subreddit
+"""
 import requests
+import time
 
 
 def number_of_subscribers(subreddit):
-    """count subreddit subscribers"""
-
-    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;'
-                  'rv: 123.0) Gecko/20100101 Firefox/123.0'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=user_agent)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    """
+    Subreddit subscribers count
+    """
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    headers = {"User-Agent": "TSubs/1.0 (by Wills)"}
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()
+        subs = data["data"]["subscribers"]
+        return subs
+    return 0
